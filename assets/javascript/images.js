8365a48862290336e425ac632b816350eed62cd1
Sarah B <smblair87@gmail.com>
2019-04-10 20:48:54 -0700

    Adding updated JavaScript

--- assets/javascript/images.js
+++ assets/javascript/images.js
@@ -59,64 +59,60 @@
 // Comic Vine API Key 
 // 7e34951fcec75de14801f45cdd1fa59016a6a03c
 
+// Declares variable for queryURL
+var searchTerm = $("#search-images").text();
+console.log(searchTerm);
+
 // Function to display images
 function displayImages() {
 
-// Declares variable for queryURL
-var image = $(text).attr("search-images");
-console.log(image);
+// Query URL for Comic Vine
+var queryURL = "https://comicvine.gamespot.com/api/volumes/?api_key=7e34951fcec75de14801f45cdd1fa59016a6a03c&format=json&sort=name:asc&filter=name:" + searchTerm + "";
+console.log(queryURL);
 
-        // Query URL for Comic Vine
-        var queryURL = "https://comicvine.gamespot.com/api/volumes/?api_key=7e34951fcec75de14801f45cdd1fa59016a6a03c&format=json&" + image + "&limit=10";
-        console.log(queryURL);
+// Ajax call 
+$.ajax({
+    url: queryURL,
+    method: "GET"
 
-        // Ajax call 
-        $.ajax({
-            url: queryURL,
-            method: "GET"
+}).then(function (response) {
 
-        }).then(function (response) {
+// Testing response to make sure object is there
+console.log(response);
 
-            // Testing response to make sure object is there
-            console.log(response);
+// Sets up variable for response data
+var results = response.data;
 
-            // Sets up variable for response data
-            var results = response.data;
+// Empties div 
+$("#images").empty();
 
-            // Empties div 
-            $("#images").empty();
+// For loop to display images
+for (var i = 0; i < results.length; i++) {
 
-            // For loop to display images
-            for (var i = 0; i < results.length; i++) {
+    // Adds results to div
+    $("#images").append(results);
 
-                // Adds results to div
-                $("#images").append(results);
-
-            };
-        });
-    };
+        };
+    });
+};
 
 
+// Function to add click event when submit button is clicked
+$("#submit-button").on("click", function (event) {
 
+event.preventDefault();
 
+// Takes user's input in text box and trims it so there's no white space
+var searchTerm = $("#submit-button").text().trim();
 
-    // Function to add click event when submit button is clicked
-    $("#submit-button").on("click", function (event) {
+        // Pushes user's input to array of search terms
+        searchTerm.push(searchTerm);
 
-        event.preventDefault();
-
-        // Takes user's input in text box and trims it so there's no white space
-        var image = $("#userbutton").val().trim();
-
-        // Pushes user's input to array of topics
-        image.push(image);
-
-        // Calls make buttons function to show user's submission
-        showButtons();
+       
     });
 
 
 
-});
+
 
 
