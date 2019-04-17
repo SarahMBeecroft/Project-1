44513fdd5a1efdcb00948edd664691224a8eb992
Sarah B <smblair87@gmail.com>
2019-04-17 16:45:32 -0700

    Updated firebase code and linked to it from index, added a readme

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# Project-1
+# Project-1 - SuperSource
 Group project #1
+
+### Team Members
+- Sarah Beecroft
+- Dongdong Zhang
+- Jon Lindley
+
+### Tools and APIs Utilized
+- HTML
+- CSS
+- JavaScript
+- Comicvine API
+- iTunes API
+- SuperHero API
+- Yelp API
+- Google Maps API
+- iTunes API
+- Firebase
+- Adobe Photoshop
+
+### App Description
+SuperSource is your all-in-one comic book content resource! Just enter in a search term (your favorite superhero, comic book, etc.),
+and hit enter or press submit to get quick character, image, movie, music, and store results. 
+
+
+#### Link to SuperSource
+https://sarahmbeecroft.github.io/Project-1/
--- assets/javascript/firebase.js
+++ assets/javascript/firebase.js
@@ -1,30 +1,30 @@
 // Initializes Firebase
-var config = {
+  var config = {
     apiKey: "AIzaSyAA7fqbxbh_t9fqSWqsFGcoA1zS5etAR5o",
     authDomain: "supersearch-b7f54.firebaseapp.com",
     databaseURL: "https://supersearch-b7f54.firebaseio.com",
     projectId: "supersearch-b7f54",
     storageBucket: "supersearch-b7f54.appspot.com",
     messagingSenderId: "867218048475"
-};
-firebase.initializeApp(config);
+  };
+  firebase.initializeApp(config);
 
 // Creates a variable to reference the database
 var database = firebase.database();
 
 // Creates click event when user clicks submit button 
-$("#searchAll").on("click", function (event) {
+$("#submit-button").on("click", function (event) {
 
     // Prevents page from being reloaded
     event.preventDefault();
 
     // Gets input values
-    var userInput = $("#search-images").val().trim();
+    var userInput = $("#searchAll").val().trim();
 
     // Creates local "temporary" object for holding comic data
     var comicData = {
 
-        userInput: userInput,
+        FBuserInput: userInput,
         dateAdded:
         firebase.database.ServerValue.TIMESTAMP
     };
